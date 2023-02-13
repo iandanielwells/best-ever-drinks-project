e34ba864170769d104a39dee3a7f1e5df72bbfdd
Teddy Economou <teddyecon@gmail.com>
2023-02-13 14:40:20 -0500

    search name working

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Headers from "./Components/Headers";
 import DrinksList from "./Components/DrinksList";
+import Search from "./Components/Search";
 
 function App() {
   const [drinks, setDrinks] = useState([]);
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     fetch(" http://localhost:3000/cocktails")
       .then((resp) => resp.json())
       .then((data) => setDrinks(data));
   }, []);
+
   return (
     <div className="app">
       <header className="App-header">
         <Headers />
-        <DrinksList drinks={drinks} />
+        <Search 
+        search={search}
+        setSearch={setSearch}/>
+        <DrinksList 
+        drinks={drinks} 
+        search={search}/>
       </header>
     </div>
   );
