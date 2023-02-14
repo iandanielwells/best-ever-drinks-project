e20217cc56f30081779d811a6e075837f6430f4a
Luana <lugamoreira@gmail.com>
2023-02-14 11:26:07 -0500

    updating

--- src/Components/Headers.js
+++ src/Components/Headers.js
@@ -1,10 +1,43 @@
-function Headers() {
-    return (
-      <header>
-        <nav>
-          <h1>FlatIron Bar</h1>
-        </nav>
-      </header>
-    );
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function NavScrollExample({ setSearch }) {
+  function handleChange(event) {
+    setSearch(event.target.value);
   }
-  export default Headers;
+
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">FlatIron Bar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action2">Build your Own Drink</Nav.Link>
+            <Nav.Link href="#action2">Favorite Drinks</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              onChange={handleChange}
+              type="search"
+              placeholder="Search Drinks"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
